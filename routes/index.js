536d64c333d68e12d168d53ac8ac6a02ac60c0c8
snsb08 <76939844+snsb08@users.noreply.github.com>
2023-04-28 17:56:31 -0400

    lesson 4 assignment

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,9 @@
 const routes = require('express').Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
+
+routes.use('/api-docs', swaggerUi.serve);
+routes.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 const controller1 = require('../controllers');
 
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 
+
 const app = express();
 
 //to use with Render. either a render port from environment variable or local whichever available
