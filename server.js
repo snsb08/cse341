a5fd5acf51ece5594ab387919b58c5cae34f40d1
snsb08 <76939844+snsb08@users.noreply.github.com>
2023-04-28 20:42:33 -0400

    update L4

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
   })
   .use('/', require('./routes'));
